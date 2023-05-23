edc2b4bf6ef8d31b9f94778e24f70d4aec8576c8
Tantan Andriansyah <tantan.a@smt-id.com>
2023-05-24 01:30:53 +0700

    :ambulance: Hotfix: validation create user

--- src/middleware/validator/users.js
+++ src/middleware/validator/users.js
@@ -5,34 +5,7 @@ const createUserValidation = () => {
         check('email', 'Email harus diisi').notEmpty(),
         check('email', 'Format email tidak sesuai').isEmail(),
         check('first_name', 'Nama depan harus diisi').notEmpty(),
-        check('roles', 'Hak akses harus dipilih').notEmpty(),
-        check('password', 'Kata sandi harus diisi').optional({ checkFalsy: true }),
-        check('password', 'Kata sandi Anda harus setidaknya 10 karakter serta mengandung setidaknya satu huruf besar, satu huruf kecil, satu angka, dan satu karakter khusus.')
-            .isLength({ min: 12, max: 32 })
-            .matches(/^(?=.*[a-z]).{10,50}$/, "i")
-            .matches(/^(?=.*[0-9]).{10,50}$/, "i")
-            .matches(/^(?=.*[!@#$&*]).{10,50}$/, "i")
-            .matches(/^(?=.*[A-Z]).{10,50}$/, "i"),
-        check('area_polda').custom((value, { req }) => {
-            // Check role value
-            if (req.body.roles == 4 || (req.body.roles == 5 && (req.body.role_input == 'super_admin' || req.body.role_input == 'admin_pusat'))) {
-                if (!value) {
-                    return Promise.reject('Wilayah harus Diisi');
-                }
-            }
-
-            return true;
-        }),
-        check('area_polres').custom((value, { req }) => {
-            // Check role value
-            if (req.body.roles == 7) {
-                if (!value) {
-                    return Promise.reject('Wilayah harus Diisi');
-                }
-            }
-
-            return true;
-        }),
+        check('roles', 'Hak akses harus dipilih').notEmpty()
     ]
 }
 
